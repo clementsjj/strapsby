46e9bcc282e38914680b852880a50fe07e4a37ca
JJ Clements <clementsjj@gmail.com>
2020-04-11 03:41:13 -0400

    Create install.sh

---
+++ server/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo $HOME
+printf "#####\nINSTALLING NODEJS\n#####"
+
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
+sudo apt-get install nodejs
+mkdir ~/.npm-global
+npm config set prfix '~/.npm-global'
+printf "#### \n SET NPM CONFIG \n ####\n"
+echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
+
+sudo apt-get install sqlite3
+sudo apt update
+sudo apt install -y nginx
+
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 'OpenSSH'
+sudo ufw allow 22/tcp
+sudo ufw enable
+sudo ufw status
